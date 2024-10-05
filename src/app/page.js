1a77290a42b1248e8946a4da9d7551894f8754a3
Minakoss <nontaspapastefanis@gmail.com>
2024-10-05 21:14:09 +0300

    update intro

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use client";
+"use client"; // Ensure the code runs on the client-side
 
 import { useState, useEffect } from "react";
 import Link from "next/link"; // Import Link from next/link
@@ -24,7 +24,7 @@ export default function Home() {
 
     window.addEventListener("mousemove", handleMouseMove);
 
-    // Αφαιρούμε το intro μετά από 3 δευτερόλεπτα
+    // Αφαιρούμε το intro μετά από 1.5 δευτερόλεπτα
     const introTimeout = setTimeout(() => {
       setIntroVisible(false);
     }, 1500);
@@ -46,136 +46,144 @@ export default function Home() {
     >
       {/* Intro Section */}
       {isIntroVisible && (
-        <div className="fixed inset-0 flex items-center justify-center bg-[#0A192F] z-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-[#0A192F] z-50 animate-pixel-fade">
           <h1 className="text-5xl md:text-7xl font-bold text-white animate-fade">
             WebCraft
           </h1>
         </div>
       )}
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 container mx-auto">
-        {/* Αριστερή στήλη */}
-        <div className="flex flex-col justify-center space-y-4 md:sticky top-0 h-screen">
-          <h1 className="text-2xl md:text-4xl font-bold text-white">
-            WebCraft
-          </h1>
-          <h2 className="text-md md:text-xl font-medium text-gray-300">
-            front-end developer Services
-          </h2>
-          <p className="text-md text-gray-400">
-            I build pixel-perfect, engaging, and accessible digital experiences.
-          </p>
-
-          {/* Μενού πλοήγησης */}
-          <nav className="mt-4 md:mt-8">
-            <ul className="space-y-6">
-              <li className="group text-md font-medium text-gray-400 hover:text-white cursor-pointer">
-                <Link href="/projects">
-                  <span className="relative">
-                    Projects
-                    <span className="absolute left-0 bottom-0 w-full h-[1px] bg-gray-400 opacity-0 group-hover:opacity-100 group-hover:h-[2px] transition-all duration-300"></span>
-                  </span>
-                </Link>
-              </li>
-              <li className="group text-md font-medium text-gray-400 hover:text-white cursor-pointer">
-                <Link href="/connect">
-                  <span className="relative">
-                    Connect
-                    <span className="absolute left-0 bottom-0 w-full h-[1px] bg-gray-400 opacity-0 group-hover:opacity-100 group-hover:h-[2px] transition-all duration-300"></span>
-                  </span>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <div className="flex space-x-4 mt-8 justify-left">
-            <Link href="https://github.com/Minakoss" target="blank">
-              <FaGithub className="text-gray-400 hover:text-white text-2xl cursor-pointer transition-colors duration-300" />
-            </Link>
-            <Link href="https://linkedin.com" target="blank">
-              <FaLinkedin className="text-gray-400 hover:text-white text-2xl cursor-pointer transition-colors duration-300" />
-            </Link>
-            <Link href="https://codepen.io/Minakoss" target="blank">
-              <FaCodepen className="text-gray-400 hover:text-white text-2xl cursor-pointer transition-colors duration-300" />
-            </Link>
-            <Link href="https://instagram.com" target="blank">
-              <FaInstagram className="text-gray-400 hover:text-white text-2xl cursor-pointer transition-colors duration-300" />
-            </Link>
-
-            <Link href="https://twitter.com" target="blank">
-              <FaDiscord className="text-gray-400 hover:text-white text-2xl cursor-pointer transition-colors duration-300" />
-            </Link>
-          </div>
-        </div>
-
-        {/* Δεξιά στήλη */}
-        <div className="text-md space-y-4 mt-10 md:mt-20 overflow-y-scroll h-screen">
-          <div className="text-gray-400">
-            <h1 className="text-lg md:text-xl font-bold text-white">
-              Welcome to WebCraft
+      <div
+        className={`transition-opacity duration-1000 ${
+          isIntroVisible ? "opacity-0" : "opacity-100"
+        }`}
+      >
+        {/* Εδώ ξεκινάει το περιεχόμενο της σελίδας */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 container mx-auto">
+          {/* Αριστερή στήλη */}
+          <div className="flex flex-col justify-center space-y-4 md:sticky top-0 h-screen">
+            <h1 className="text-2xl md:text-4xl font-bold text-white">
+              WebCraft
             </h1>
-            <p>
-              At WebCraft, we bring your digital ideas to life through modern,
-              innovative, and beautifully crafted websites and applications. Our
-              team of expert developers and designers specializes in creating
-              high-performance, user-friendly web solutions that are not only
-              functional but visually captivating.
+            <h2 className="text-md md:text-xl font-medium text-gray-300">
+              front-end developer Services
+            </h2>
+            <p className="text-md text-gray-400">
+              I build pixel-perfect, engaging, and accessible digital
+              experiences.
             </p>
-          </div>
-          <p className="text-gray-400">
-            We believe that design is more than just aesthetics—it’s about
-            creating seamless and engaging user experiences that connect with
-            audiences on every device. Whether you need a dynamic website, an
-            interactive web application, or a bespoke digital solution, we
-            combine cutting-edge technology with the latest design trends to
-            deliver products that stand out. Explore our portfolio of
-            pixel-perfect projects and let us help you build the future of your
-            digital presence.
-          </p>
-          <div className="text-gray-400">
-            <h3 className="text-lg text-white">Services We Offer:</h3>
-            <ul className="list-disc ml-4 md:ml-6">
-              <li>Custom website development</li>
-              <li>Web and mobile application design</li>
-              <li>UI/UX design and consultation</li>
-              <li>Responsive design for all devices</li>
-              <li>E-commerce solutions</li>
-              <li>Content management systems</li>
-              <li>Social content/management</li>
-            </ul>
+
+            {/* Μενού πλοήγησης */}
+            <nav className="mt-4 md:mt-8">
+              <ul className="space-y-6">
+                <li className="group text-md font-medium text-gray-400 hover:text-white cursor-pointer">
+                  <Link href="/projects">
+                    <span className="relative">
+                      Projects
+                      <span className="absolute left-0 bottom-0 w-full h-[1px] bg-gray-400 opacity-0 group-hover:opacity-100 group-hover:h-[2px] transition-all duration-300"></span>
+                    </span>
+                  </Link>
+                </li>
+                <li className="group text-md font-medium text-gray-400 hover:text-white cursor-pointer">
+                  <Link href="/connect">
+                    <span className="relative">
+                      Connect
+                      <span className="absolute left-0 bottom-0 w-full h-[1px] bg-gray-400 opacity-0 group-hover:opacity-100 group-hover:h-[2px] transition-all duration-300"></span>
+                    </span>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <div className="flex space-x-4 mt-8 justify-left">
+              <Link href="https://github.com/Minakoss" target="blank">
+                <FaGithub className="text-gray-400 hover:text-white text-2xl cursor-pointer transition-colors duration-300" />
+              </Link>
+              <Link href="https://linkedin.com" target="blank">
+                <FaLinkedin className="text-gray-400 hover:text-white text-2xl cursor-pointer transition-colors duration-300" />
+              </Link>
+              <Link href="https://codepen.io/Minakoss" target="blank">
+                <FaCodepen className="text-gray-400 hover:text-white text-2xl cursor-pointer transition-colors duration-300" />
+              </Link>
+              <Link href="https://instagram.com" target="blank">
+                <FaInstagram className="text-gray-400 hover:text-white text-2xl cursor-pointer transition-colors duration-300" />
+              </Link>
+
+              <Link href="https://twitter.com" target="blank">
+                <FaDiscord className="text-gray-400 hover:text-white text-2xl cursor-pointer transition-colors duration-300" />
+              </Link>
+            </div>
           </div>
 
-          <div className="mt-6 md:mt-8">
-            <h3 className="text-lg text-white">
-              Let WebCraft transform your vision into reality. Together, we can
-              build something remarkable.
-            </h3>
+          {/* Δεξιά στήλη */}
+          <div className="text-md space-y-4 mt-10 md:mt-20 overflow-y-scroll h-screen">
+            <div className="text-gray-400">
+              <h1 className="text-lg md:text-xl font-bold text-white">
+                Welcome to WebCraft
+              </h1>
+              <p>
+                At WebCraft, we bring your digital ideas to life through modern,
+                innovative, and beautifully crafted websites and applications.
+                Our team of expert developers and designers specializes in
+                creating high-performance, user-friendly web solutions that are
+                not only functional but visually captivating.
+              </p>
+            </div>
             <p className="text-gray-400">
-              This text positions your brand as a forward-thinking developer of
-              modern websites and applications with an emphasis on high-quality
-              design. Feel free to adjust it to match your brand’s tone or
-              specific services!
+              We believe that design is more than just aesthetics—it’s about
+              creating seamless and engaging user experiences that connect with
+              audiences on every device. Whether you need a dynamic website, an
+              interactive web application, or a bespoke digital solution, we
+              combine cutting-edge technology with the latest design trends to
+              deliver products that stand out. Explore our portfolio of
+              pixel-perfect projects and let us help you build the future of
+              your digital presence.
             </p>
+            <div className="text-gray-400">
+              <h3 className="text-lg text-white">Services We Offer:</h3>
+              <ul className="list-disc ml-4 md:ml-6">
+                <li>Custom website development</li>
+                <li>Web and mobile application design</li>
+                <li>UI/UX design and consultation</li>
+                <li>Responsive design for all devices</li>
+                <li>E-commerce solutions</li>
+                <li>Content management systems</li>
+                <li>Social content/management</li>
+              </ul>
+            </div>
 
-            {/* Τεχνολογίες */}
-            <div className="flex flex-wrap space-x-2 md:space-x-2 mt-4">
-              <span className="px-2 py-1 text-sm md:px-4 md:py-2 md:text-base bg-[#0A192F] text-[#66E0FF] rounded-full shadow-md hover:bg-[#0A2540]">
-                HTML/CSS
-              </span>
-              <span className="px-2 py-1 text-sm md:px-4 md:py-2 md:text-base bg-[#0A192F] text-[#66E0FF] rounded-full shadow-md hover:bg-[#0A2540]">
-                JavaScript
-              </span>
-              <span className="px-2 py-1 text-sm md:px-4 md:py-2 md:text-base bg-[#0A192F] text-[#66E0FF] rounded-full shadow-md hover:bg-[#0A2540]">
-                React
-              </span>
-              <span className="px-2 py-1 text-sm md:px-4 md:py-2 md:text-base bg-[#0A192F] text-[#66E0FF] rounded-full shadow-md hover:bg-[#0A2540]">
-                WordPress
-              </span>
+            <div className="mt-6 md:mt-8">
+              <h3 className="text-lg text-white">
+                Let WebCraft transform your vision into reality. Together, we
+                can build something remarkable.
+              </h3>
+              <p className="text-gray-400">
+                This text positions your brand as a forward-thinking developer
+                of modern websites and applications with an emphasis on
+                high-quality design. Feel free to adjust it to match your
+                brand’s tone or specific services!
+              </p>
+
+              {/* Τεχνολογίες */}
+              <div className="flex flex-wrap space-x-2 md:space-x-2 mt-4">
+                <span className="px-2 py-1 text-sm md:px-4 md:py-2 md:text-base bg-[#0A192F] text-[#66E0FF] rounded-full shadow-md hover:bg-[#0A2540]">
+                  HTML/CSS
+                </span>
+                <span className="px-2 py-1 text-sm md:px-4 md:py-2 md:text-base bg-[#0A192F] text-[#66E0FF] rounded-full shadow-md hover:bg-[#0A2540]">
+                  JavaScript
+                </span>
+                <span className="px-2 py-1 text-sm md:px-4 md:py-2 md:text-base bg-[#0A192F] text-[#66E0FF] rounded-full shadow-md hover:bg-[#0A2540]">
+                  React
+                </span>
+                <span className="px-2 py-1 text-sm md:px-4 md:py-2 md:text-base bg-[#0A192F] text-[#66E0FF] rounded-full shadow-md hover:bg-[#0A2540]">
+                  WordPress
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Προσθήκη Footer */}
+        {/* Προσθήκη Footer */}
+      </div>
     </div>
   );
 }
