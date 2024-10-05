29606b939e6c290add3227549ab2dc04255d9796
Minakoss <nontaspapastefanis@gmail.com>
2024-10-05 19:18:39 +0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link"; // Import Link from next/link
 import Footer from "./components/Footer"; // Import Footer
+
 import {
   FaGithub,
   FaLinkedin,
